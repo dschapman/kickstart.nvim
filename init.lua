1c620c8fe1ccddfcd43d280e483d01c5c16c96ea
Daniel Chapman <scriptor.deo@gmail.com>
2025-02-21 16:38:49 -0500

    update packages

--- init.lua
+++ init.lua
@@ -808,7 +808,7 @@ require('lazy').setup({
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('nord').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
