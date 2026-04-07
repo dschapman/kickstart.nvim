7ee8856c71d164fce33942e91382febd94c27581
Daniel Chapman <daniel@dschapman.com>
2026-04-06 22:33:18 -0400

    bump

--- init.lua
+++ init.lua
@@ -508,6 +508,53 @@ require('lazy').setup({
         end,
       })
 
+      -- Diagnostic Config
+      -- See :help vim.diagnostic.Opts
+      vim.diagnostic.config {
+        severity_sort = true,
+        float = { border = 'rounded', source = 'if_many' },
+        underline = { severity = vim.diagnostic.severity.ERROR },
+        signs = vim.g.have_nerd_font and {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = '󰀪 ',
+            [vim.diagnostic.severity.INFO] = '󰋽 ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
+          },
+        } or {},
+        virtual_text = {
+          source = 'if_many',
+          spacing = 2,
+          format = function(diagnostic)
+            local diagnostic_message = {
+              [vim.diagnostic.severity.ERROR] = diagnostic.message,
+              [vim.diagnostic.severity.WARN] = diagnostic.message,
+              [vim.diagnostic.severity.INFO] = diagnostic.message,
+              [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
+          end,
+        },
+      }
+
+      -- LSP servers and clients are able to communicate to each other what features they support.
+      --  By default, Neovim doesn't support everything that is in the LSP specification.
+      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+      local matlab_install_path = vim.env.MATLAB_HOME
+      if not matlab_install_path or matlab_install_path == '' then
+        local matlab_apps = vim.fn.glob('/Applications/MATLAB*.app', false, true)
+        table.sort(matlab_apps)
+        matlab_install_path = matlab_apps[#matlab_apps]
+      end
+
+      local matlab_settings = {}
+      if matlab_install_path and matlab_install_path ~= '' then
+        matlab_settings.installPath = matlab_install_path
+      end
+
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --  See `:help lsp-config` for information about keys and how to configure
@@ -528,7 +575,7 @@ require('lazy').setup({
 
         matlab_ls = {
           settings = {
-            MATLAB = { installPath = '/Applications/MATLAB_R2025b.app' },
+            MATLAB = matlab_settings,
           },
         },
 
--- lua/custom/plugins/matlab.lua
+++ lua/custom/plugins/matlab.lua
@@ -1,15 +1,111 @@
 return {
   {
-    'idossha/matlab.nvim',
-    ft = 'matlab',
+    'Vigemus/iron.nvim',
+    ft = { 'matlab', 'octave' },
+    init = function()
+      vim.filetype.add {
+        extension = {
+          m = 'matlab',
+        },
+      }
+    end,
     config = function()
-      require('matlab').setup {
-        executable = 'matlab',
-        auto_start = true,
-        default_mappings = true,
+      local iron = require 'iron.core'
+      local view = require 'iron.view'
+      local ok_which_key, which_key = pcall(require, 'which-key')
+
+      local function matlab_app_path()
+        local install_path = vim.env.MATLAB_HOME
+        if install_path and install_path ~= '' then
+          return install_path
+        end
+
+        local matlab_apps = vim.fn.glob('/Applications/MATLAB*.app', false, true)
+        table.sort(matlab_apps)
+        return matlab_apps[#matlab_apps]
+      end
+
+      local function matlab_command()
+        local matlab = vim.fn.exepath 'matlab'
+        if matlab ~= '' then
+          return { matlab, '-nosplash', '-nodesktop' }
+        end
+
+        local app_path = matlab_app_path()
+        if app_path and app_path ~= '' then
+          return { app_path .. '/bin/matlab', '-nosplash', '-nodesktop' }
+        end
+
+        local octave = vim.fn.exepath 'octave'
+        if octave ~= '' then
+          return { octave, '--interactive', '--quiet' }
+        end
+
+        return { 'matlab', '-nosplash', '-nodesktop' }
+      end
+
+      iron.setup {
+        config = {
+          scratch_repl = true,
+          repl_definition = {
+            matlab = {
+              command = matlab_command,
+              block_dividers = { '%%' },
+            },
+            octave = {
+              command = matlab_command,
+              block_dividers = { '%%' },
+            },
+          },
+          repl_filetype = function(_, ft)
+            return ft
+          end,
+          repl_open_cmd = {
+            view.split.rightbelow(0.35),
+            view.split.vertical.botright(0.42),
+          },
+        },
+        keymaps = {
+          toggle_repl = '<leader>mr',
+          send_motion = '<leader>ms',
+          visual_send = '<leader>ms',
+          send_file = '<leader>mf',
+          send_line = '<leader>ml',
+          interrupt = '<leader>mi',
+        },
+        highlight = {
+          italic = true,
+        },
+        ignore_blank_lines = true,
       }
 
-      vim.keymap.set('n', '<leader>ms', '<cmd>MatlabStartServer<cr>', { desc = '[M]atlab [S]tart server' })
+      if ok_which_key then
+        which_key.add {
+          { '<leader>m', group = '[M]ATLAB' },
+        }
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('custom-matlab-keymaps', { clear = true }),
+        pattern = { 'matlab', 'octave' },
+        callback = function(event)
+          vim.keymap.set('n', '<leader>mc', function()
+            iron.send_code_block(false)
+          end, {
+            buffer = event.buf,
+            desc = 'Send MATLAB cell',
+            silent = true,
+          })
+
+          vim.keymap.set('n', '<leader>mn', function()
+            iron.send_code_block(true)
+          end, {
+            buffer = event.buf,
+            desc = 'Send MATLAB cell and move',
+            silent = true,
+          })
+        end,
+      })
     end,
   },
 }
